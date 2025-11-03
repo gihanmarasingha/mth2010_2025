cb54cd99eb77c0d7013ac10956d4488c1f301ae3
Gihan Marasingha <gihan.marasingha@gmail.com>
2025-11-03 16:19:49 +0000

    permutation representations

---
+++ Mth20102025/Week07_01.lean
@@ -0,0 +1,97 @@
+import Mathlib
+
+/-
+# Permutation representations
+-/
+
+/-
+## Recap of group actions
+Let `(G, *)` be a group and `A` a set (or type in Lean). We denote by
+`• : G × A → A` a group action satisfying the properties
+
+* `1 • a = a` for all `a ∈ A` and
+* `(g₁ * g₂) • a = g₁ • (g₂ • a)` for all `g₁, g₂ ∈ G` and `a ∈ A`.
+
+This is represented in Lean as follows.
+
+**Note** that `•` is typed as `\bu` or `\smul`.
+-/
+
+variable {G A : Type*} [Group G] [MulAction G A]
+
+example (a : A) : (1 : G) • a = a := by
+  simp
+
+example (g₁ g₂ : G) (a : A) : (g₁ * g₂) • a = g₁ • (g₂ • a) := by
+  rw [mul_smul]
+
+/-
+# Permutations from Group Actions
+
+We define `τₕ : A → A` by `τₕ(a) = h • a`
+-/
+
+def τ (g : G) : A → A := fun a => g • a
+
+/-
+Then `τₕ` is a bijection for each `h ∈ G`.
+
+One way to prove this is to define a function `σₕ` , for each `h ∈ G`, and show that
+`σₕ` is a left inverse of `τₕ` and that `σₕ` is a right inverse of `τₕ`.
+-/
+
+open Function
+
+def σ (g : G) : A → A := sorry
+
+lemma left_inverse (g : G) : LeftInverse (σ g) (τ g : A → A) := by
+  sorry
+
+lemma right_inverse (g : G) : RightInverse (σ g) (τ g : A → A) := by
+  sorry
+
+lemma tau_bijective (g : G) : Bijective (τ g : A → A) := by
+  sorry
+
+/-
+The permutation representation is the map `π : G → Symm(A)` (where Symm(A) is the symmetry
+group of `A`) given by `π(h) = τₕ`.
+
+In Lean, we write `Equiv.Perm A` for `Symm(A)`.
+
+Note that to specify an element of `Equiv.Perm A` is to specify a function
+`toFun : A → A`, specify an inverse `invFun : A → A` and give proofs `left_inv` and
+`right_inv`.
+-/
+
+/-
+## The permutation representation
+-/
+
+def π (g : G) : Equiv.Perm A where
+  toFun := τ g
+  invFun := sorry
+  left_inv := by
+    unfold LeftInverse
+    sorry
+  right_inv := by
+    unfold RightInverse
+    sorry
+
+/-
+## The permutation representation as a group homomorphism
+
+To show the permtuation representation is a homomorphism in Lean, we must show
+
+* `π 1 = 1` and
+* `π (x * y) = π x * π y` for all `x, y ∈ G`
+
+Note in ordinary mathematics, we get `π 1 = 1` for free.
+-/
+
+def permRep : G →* Equiv.Perm A :=
+{ toFun    := π,
+  map_one' := by
+    sorry
+  map_mul' := by
+    sorry }  -- `
