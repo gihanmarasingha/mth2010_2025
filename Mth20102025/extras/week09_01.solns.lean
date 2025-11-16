b7f30cb33cad9722d5138e6c6d4d16594301e9be
Gihan Marasingha <gihan.marasingha@gmail.com>
2025-11-16 19:20:27 +0000

    vring proof

---
+++ Mth20102025/extras/week09_01.solns.lean
@@ -0,0 +1,113 @@
+import Mathlib
+
+/-
+A certain subring of a field.
+-/
+
+open Function
+
+structure Subring' (R : Type*) [Ring R] extends AddSubgroup R, Subsemigroup R
+
+structure VFunc (K : Type*) [Field K] where
+  f : K → WithTop ℤ
+  zero_val : ∀ x, f x = ⊤ ↔ x = 0
+  surj' : f.Surjective
+  mul_add' : ∀ x y, f (x * y) = f x + f y
+  add_ge_min' : ∀ x y, f (x + y) ≥ min (f x) (f y)
+
+namespace VFunc
+
+variable {K} [Field K] (v : VFunc K)
+
+lemma ne_zero_val_int {x : K} (hxnz : x ≠ 0) : ∃ (n : ℤ), v.f x = n := by
+  have hne : v.f x ≠ ⊤ := by
+    intro h
+    rw [v.zero_val] at h
+    exact hxnz h
+  exact Option.ne_none_iff_exists'.mp hne
+
+lemma one_val : v.f 1 = 0 := by
+  rcases (ne_zero_val_int v (one_ne_zero)) with ⟨n, hn⟩
+  have hf : v.f 1 + v.f 1 = v.f 1 + 0 :=
+    calc v.f 1 + v.f 1 = v.f (1 * 1) := by rw [v.mul_add']
+    _ = v.f 1 + 0 := by norm_num
+  have hneq : n + n = n + 0 := by
+    rw [hn] at hf
+    exact_mod_cast hf
+  have hnz : n = 0 := by apply add_left_cancel hneq
+  rw [hn, hnz]
+  norm_num
+
+lemma neg_one_val : v.f (-1) = 0 := by
+  have h : v.f 1 = v.f (-1) + v.f (-1) :=
+    calc v.f 1 = v.f ((-1) * (-1)) := by norm_num
+    _ = v.f (-1) + v.f (-1) := by rw [v.mul_add']
+  rw [one_val v] at h
+  have hnone : (-1 : K) ≠ 0 := by
+    refine neg_ne_zero.mpr ?_
+    exact one_ne_zero
+  rcases (ne_zero_val_int v hnone) with ⟨n, hn⟩
+  have hnsum : 0 = n + n := by
+    rw [hn] at h
+    exact_mod_cast h
+  have hnz : n = 0 := by
+    linarith
+  rw [hn]
+  exact_mod_cast hnz
+
+lemma neg_val {x : K} : v.f (-x) = v.f x := by
+  calc v.f (-x ) = v.f ((-1) * x) := by norm_num
+    _ = v.f (-1) + v.f x := by rw [v.mul_add']
+    _ = 0 + v.f x := by rw [neg_one_val]
+    _ = v.f x := by rw [zero_add]
+
+def vring : Subring' K where
+  carrier := {x : K | v.f (x : K) ≥ 0}
+  add_mem' := by
+    intro a b (ha : v.f a ≥ 0) (hb : v.f b ≥ 0)
+    show v.f (a + b) ≥ 0
+    calc v.f (a + b) ≥ min (v.f a) (v.f b) := by apply v.add_ge_min'
+    _ ≥ 0 := by exact le_min ha hb -- Found using `apply?`
+  zero_mem' := by
+    show v.f 0 ≥ 0
+    have hz : v.f (0 : K) = ⊤ := by rw [v.zero_val]
+    rw [hz]
+    exact OrderTop.le_top 0
+  neg_mem' := by
+    intro a (ha : v.f a ≥ 0)
+    show v.f (-a) ≥ 0
+    have h : -a = (-1) * a := by exact neg_eq_neg_one_mul a
+    rw [h]
+    rw [v.mul_add']
+    rw [neg_one_val, zero_add]
+    exact ha
+  mul_mem' := by
+    intro a b ha hb
+    simp only [Set.mem_setOf_eq] at * -- Find this from `simp?`
+    rw [v.mul_add']
+    exact Left.add_nonneg ha hb -- Find via `apply?`
+
+/- lemma val_inv (x : K) (hnz : x ≠ 0) : v.f x⁻¹ = - v.f x := by
+  rcases (ne_zero_val_int v hnz) with ⟨n, hn⟩
+  rcases (ne_zero_val_int v (inv_ne_zero hnz)) with ⟨m, hm⟩
+  have hinu : ∃ z : Kˣ, z = x := by
+    refine Units.exists0.mpr ?_
+    use x, hnz
+    exact rfl
+  rcases hinu with ⟨z, hz⟩
+  have h : 0 = v.f x + v.f x⁻¹ :=
+    calc 0 = v.f 1 := by rw [one_val]
+    _ = v.f (z * z⁻¹) := by simp
+    _ = v.f z + v.f z⁻¹ := by rw [v.mul_add']; norm_cast
+    _ = v.f x + v.f x⁻¹ := by rw [hz]
+
+  calc v.f x⁻¹ = 0 + v.f x⁻¹ := by rw [zero_add]
+  _ = (-(v.f x) + v.f x) + v.f x⁻¹ := by rw [neg_add_cancel (v.f x)]
+  _ = - v.f x := by sorry -/
+
+lemma mem_or_inv_mem (x : K) (h : x ≠ 0) : x ∈ (vring v).carrier ∨ x⁻¹ ∈ (vring v).carrier := by
+  rw [or_iff_not_imp_left]
+  intro hxnot
+  sorry
+
+end VFunc
