473a1d66cd42ddf06d5b4e4fb42920231b2d01ce
Gihan Marasingha <gihan.marasingha@gmail.com>
2025-11-16 14:09:27 +0000

    add further instructions for week 8

--- Mth20102025/Week08_01.lean
+++ Mth20102025/Week08_01.lean
@@ -32,19 +32,67 @@ example : a * 1 = a := by apply mul_one
 /-
 # Example
 
-Complete the following proof, adding lines of calculation as necessary.
-The proof in each line should be of the form `rw [axiom]`, where `axiom` is
+Complete the following proofs, adding lines of calculation as necessary.
+The proof in each line should be of the form `rw [thm]`, where `thm` is
 one of the axioms given above *or* one of the lemmas you've already proved while
 doing this sheet.
 
+**Note** in some cases you may need to do a rewrite in a different form, such as
+* `rw [thm x y]` where the variables `x` and `y` are inputs to the theorem or
+* `rw [←thm]` in which every occurrence of the right side of `thm` in the goal
+  is replaced with the left side of `thm`.
+* `rw [thm1, thm2]` in which you rewrite with multiple theorems.
 -/
 
 namespace examples
 
-lemma mul_zero (a : R) : 0 = a * 0 := by
+lemma zero_add : 0 + a = a := by
+  calc
+    0 + a = a + 0 := by rw [add_comm]
+    _ = a := by sorry
+
+lemma one_mul : 1 * a = a := by
+  calc
+    1 * a = a * 1 := by rw [mul_comm]
+    _ = a := by sorry
+
+/-
+You'll may need an additional line or two of calculation to finish the next
+proof.
+-/
+
+lemma right_distrib : (a + b) * c = a * c + b * c := by
+  calc
+    (a + b) * c = c * (a + b) := by rw [mul_comm]
+    _ = a * c + b * c := by sorry
+
+lemma neg_add_cancel : -a + a = 0 := by
+  calc
+    -a + a = a + -a := by rw [add_comm]
+    _ = 0 := by rw [add_neg_cancel]
+
+/-
+The next result requires some thinking.
+-/
+
+lemma mul_zero : a * 0 = 0 := by
+  symm
+  show 0 = a * 0
   calc
     0 = (a * 0) + -(a * 0) := by rw [add_neg_cancel]
+    _ = (a * (0 + 0)) + -(a * 0) := by rw [add_zero]
     _ = a * 0 := by sorry
+
+lemma zero_mul : 0 * a = 0 := by
+  calc
+    0 * a = sorry := by sorry
+    _ = 0 := by sorry
+
+/-
+In the proof below, we use the `suffices` tactic. We start by assuming
+`h : (-a) * b + (a * b) = 0` and try to prove the result on this assumption.
+Haing done this, it remains to prove `h`.
+-/
 
 lemma neg_mul : (-a) * b = -(a * b) := by
   suffices h : (-a) * b + (a * b) = 0 by
@@ -52,6 +100,20 @@ lemma neg_mul : (-a) * b = -(a * b) := by
     _ = -(a * b) := by sorry
   calc (-a) * b + (a * b) = sorry := by sorry
     _ = 0 := by sorry
+
+lemma mul_neg : a * (-b) = -(a * b) := by
+  calc
+    a * (-b) = (-b) * a := by rw [mul_comm]
+    _ = -(a * b) := by sorry
+
+lemma neg_neg : -(-a) = a := by
+  calc
+    -(-a) = -(-a) + 0 := by rw [add_zero]
+    _ = a := by sorry
+
+/-
+I'm leaving it to you to construct the necessary `calc` structure below.
+-/
 
 lemma neg_mul_neg : (-a) * (-b) = a * b := by
   sorry
--- Mth20102025/extras/Week08_01_solns.lean
+++ Mth20102025/extras/Week08_01_solns.lean
@@ -28,20 +28,47 @@ example : a * (b + c) = a * b + a * c := by apply left_distrib
 
 example : a * 1 = a := by apply mul_one
 
-
 /-
 # Example
 
-Complete the following proof, adding lines of calculation as necessary.
+Complete the following proofs, adding lines of calculation as necessary.
 The proof in each line should be of the form `rw [axiom]`, where `axiom` is
 one of the axioms given above *or* one of the lemmas you've already proved while
 doing this sheet.
 
+**Note** in some cases you may need to do a rewrite in a different form, such as
+* `rw [thm x y]` where the variables `x` and `y` are inputs to the theorem or
+* `rw [←thm]` in which every occurrence of the right side of `thm` in the goal
+  is replaced with the left side of `thm`.
+* `rw [thm1, thm2]` in which you rewrite with multiple theorems.
 -/
 
 namespace examples
 
-lemma mul_zero (a : R) : 0 = a * 0 := by
+lemma zero_add : 0 + a = a := by
+  calc
+    0 + a = a + 0 := by rw [add_comm]
+    _ = a := by rw [add_zero]
+
+lemma one_mul : 1 * a = a := by
+  calc
+    1 * a = a * 1 := by rw [mul_comm]
+    _ = a := by rw [mul_one]
+
+lemma right_distrib : (a + b) * c = a * c + b * c := by
+  calc
+    (a + b) * c = c * (a + b) := by rw [mul_comm]
+    _ = c * a + c * b := by rw [left_distrib]
+    _ = a * c + b * c := by rw [mul_comm a, mul_comm b]
+
+lemma neg_add_cancel : -a + a = 0 := by
+  calc
+    -a + a = a + -a := by rw [add_comm]
+    _ = 0 := by rw [add_neg_cancel]
+
+lemma mul_zero : a * 0 = 0 := by
+  symm
+  show 0 = a * 0
   calc
     0 = (a * 0) + -(a * 0) := by rw [add_neg_cancel]
     _ = (a * (0 + 0)) + -(a * 0) := by rw [add_zero]
@@ -50,42 +77,46 @@ lemma mul_zero (a : R) : 0 = a * 0 := by
     _ = a * 0 + 0 := by rw [add_neg_cancel]
     _ = a * 0 := by rw [add_zero]
 
+lemma zero_mul : 0 * a = 0 := by
+  calc
+    0 * a = a * 0 := by rw [mul_comm]
+    _ = 0 := by rw [mul_zero]
+
 lemma neg_mul : (-a) * b = -(a * b) := by
   suffices h : (-a) * b + (a * b) = 0 by
     calc (-a) * b = (-a) * b + 0 := by rw [add_zero]
     _ = (-a) * b + (a * b + -(a * b)) := by rw [add_neg_cancel]
     _ = ((-a) * b + a * b) + -(a * b) := by rw [add_assoc]
     _ = 0 + -(a * b) := by rw [h]
-    _ = -(a * b) + 0 := by rw [add_comm]
-    _ = -(a * b) := by rw [add_zero]
-  calc (-a) * b + (a * b) = b * (-a) + b * a := by rw [mul_comm _ b, mul_comm _ b]
-    _ = b * (-a + a) := by rw [left_distrib]
-    _ = b * (a + -a) := by rw [add_comm]
-    _ = b * 0 := by rw [add_neg_cancel]
-    _ = 0 := by rw [←mul_zero]
+    _ = -(a * b) := by rw [zero_add]
+  calc -a * b + a * b = (-a + a) * b := by rw [right_distrib]
+    _ = 0 * b := by rw [neg_add_cancel]
+    _ = 0 := by rw [zero_mul]
+
+lemma mul_neg : a * (-b) = -(a * b) := by
+  calc
+    a * (-b) = (-b) * a := by rw [mul_comm]
+    _ = -(b * a) := by rw [neg_mul]
+    _ = -(a * b) := by rw [mul_comm]
 
 lemma neg_neg : -(-a) = a := by
   calc
     -(-a) = -(-a) + 0 := by rw [add_zero]
-    _ = -(-a) + (a + -a) := by rw [add_neg_cancel]
-    _ = -(-a) + (-a + a) := by rw [add_comm a]
+    _ = -(-a) + (-a + a) := by rw [neg_add_cancel]
     _ = (-(-a) + -a) + a := by rw [add_assoc]
-    _ = (-a + -(-a)) + a := by rw [add_comm (-a)]
-    _ = 0 + a := by rw [add_neg_cancel]
-    _ = a + 0 := by rw [add_comm]
-    _ = a := by rw [add_zero]
+    _ = 0 + a := by rw [neg_add_cancel]
+    _ = a := by rw [zero_add]
 
 lemma neg_mul_neg : (-a) * (-b) = a * b := by
   calc
     (-a) * (-b) = -(a * (-b)) := by rw [neg_mul]
-    _ = -((-b) * a) := by rw [mul_comm]
-    _ = -(-(b * a)) := by rw [neg_mul]
-    _ = -(-(a * b)) := by rw [mul_comm]
+    _ = - (-(a * b)) := by rw [mul_neg]
     _ = a * b := by rw [neg_neg]
 
 lemma neg_eq_neg_one_mul : -a = (-1) * a := by
-  sorry
-
-
+  calc
+    -a =  1 * (-a) := by rw [one_mul]
+    _ = (-(-1)) * (-a)  := by rw [neg_neg]
+    _ = (-1) * a := by rw [neg_mul_neg]
 
 end examples
