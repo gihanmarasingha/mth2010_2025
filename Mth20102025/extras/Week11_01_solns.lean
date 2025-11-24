7a9bcff74abd531ff9ca8c5da4c1a3f4c7d78513
Gihan Marasingha <gihan.marasingha@gmail.com>
2025-11-24 03:29:35 +0000

    clean up code

--- Mth20102025/extras/Week11_01_solns.lean
+++ Mth20102025/extras/Week11_01_solns.lean
@@ -21,9 +21,9 @@ notation "√11" => sqrt11
 
 namespace Z11calculations
 
-def a : R := 5 + 3 * √11
+def a : R := ⟨5, 3⟩ -- This is `5 + 3√11`
 
-def b : R := 1 + 2 * √11
+def b : R := 1 + 2 * √11 -- A user-friendly way to express `1 + 2√11`.
 
 /-
 # Exercise:
@@ -48,15 +48,11 @@ abbrev F := ZMod 5
 
 local instance : Fact (Nat.Prime 5) := by decide
 
-#check RingHom.ker_isMaximal_of_surjective
-
 def φ : R → F := by
   rintro ⟨a, b⟩
   exact a - b
 
-#eval φ (4 + 3 * √11)
-
-lemma eleven_eq_one : (11 : F) = (1 : F) := by decide
+example : φ (4 + 3 * √11) = 1 := by rfl
 
 def f : R →+* F where
   toFun := φ
@@ -64,7 +60,8 @@ def f : R →+* F where
     simp [φ]
   map_mul' := by
     intro a b
-    simp [φ, eleven_eq_one]
+    have h1 : (11 : F) = (1 : F) := by decide
+    simp [φ, h1]
     ring
   map_zero' := by
     simp [φ]
@@ -74,27 +71,22 @@ def f : R →+* F where
     ring
 
 lemma ker_sub_I : RingHom.ker f ≤ I := by
-  rintro ⟨a, b⟩ hx
+  rintro (⟨a, b⟩) hx
   change (f ⟨a, b⟩) = 0 at hx
   dsimp [f, φ] at hx
-  norm_cast at hx
-  rw [ZMod.intCast_zmod_eq_zero_iff_dvd, dvd_def] at hx
+  rw_mod_cast [ZMod.intCast_zmod_eq_zero_iff_dvd, dvd_def] at hx
   rcases hx with ⟨c, hc⟩
-  norm_cast at hc
   have ha : a = 5 * c + b := by
     linarith
   rw [I, Ideal.mem_span_pair]
   use c, b
   rw [ha]
   ext
-  · simp
-    have h1 : 1 + √11.re = (1 : ℤ) := by decide
-    rw [h1]
+  · have h1 : 1 + √11.re = (1 : ℤ) := by decide
+    simp [h1]
     ring
-  · simp
-    have h1 : √11.im = (1 : ℤ) := by decide
-    rw [h1]
-    ring
+  · have h1 : √11.im = (1 : ℤ) := by decide
+    simp [h1]
 
 lemma I_sub_ker : I ≤ RingHom.ker f := by
   intro r hr
@@ -102,13 +94,11 @@ lemma I_sub_ker : I ≤ RingHom.ker f := by
   rcases hr with ⟨a, b, rfl⟩
   change f (a * 5 + b * (1 + √11)) = 0
   dsimp [f, φ]
-  simp
   have h1 : 1 + √11.re = (1 : F) := by decide
   have h2 : √11.im = (1 : F) := by decide
   have h3 : (11 : F) = 1 := by decide
   have h4 : (5 : F) = 0 := by decide
-  rw [h1, h2, h3, h4]
-  ring
+  simp [h1, h2, h3, h4]
 
 lemma ker_eq_I : RingHom.ker f = I := by
   apply le_antisymm
